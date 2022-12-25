dd9c2b0f020901bb8d4d8c978e87f9c62e923321
YehudaNevo <ynevo1948@gmail.com>
2022-12-25 11:14:07 +0200

    HW DNS server final.

---
+++ EX6/MY_HTTP_DNS_SERVER.py
@@ -0,0 +1,147 @@
+import socket
+from string import Template
+from scapy import *
+from scapy.all import DNS, DNSQR, IP, sr1, UDP
+
+MY_IP = '0.0.0.0'
+DNS_SERVER = "8.8.8.8"
+MY_PORT = 8853
+SOCKET_TIMEOUT = 3
+FIXED_RESPONSE = ""
+
+
+def reverse_ip(ip):
+    # Split the IP address into a list of octets
+    octets = ip.split('.')
+
+    # Reverse the list of octets
+    octets.reverse()
+
+    # Join the octets into a reversed IP string
+    reversed_ip = '.'.join(octets)
+
+    # Append ".in-addr.arpa" to the reversed IP
+    reversed_ip += '.in-addr.arpa'
+
+    return reversed_ip
+
+
+def domain_to_ip(domain):
+    # Build the DNS query packet
+    packet = IP(dst=DNS_SERVER) / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=domain))
+
+    # Send the packet and get the response
+    response = sr1(packet, verbose=0)
+
+    res = []
+    size = response.ancount
+
+    for i in range(0, size):
+        if response.an[i].type == 1:
+            res.append(response.an[i].rdata)
+
+    return create_nice_html(res, "List of IPs:")
+
+
+def ip_to_domains(ip):
+    r_ip = reverse_ip(ip)
+    # Build the DNS query packet
+    packet = IP(dst=DNS_SERVER) / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=r_ip, qtype="PTR"))
+
+    # Send the packet and get the response
+    try:
+        response = sr1(packet, verbose=0)
+    except:
+        response = ""
+    # response.show()
+    res = []
+    size = response.ancount
+    # response.show()
+    for i in range(size):
+        if response.an[i].type == 12:  # ptr req..
+            res.append(response.an[i].rdata.decode())
+    return create_nice_html(res, "List of hosts names:")
+
+
+def create_nice_html(arr, name):
+    lst_template = Template("<li>  $ele </li>")
+    ele_arr = "".join([lst_template.substitute(ele=ele) for ele in arr])
+    template_html = Template("""
+    <html>
+    <head> 
+    <style>  
+     body{
+     background-color: rgb(248, 236, 209);
+                }           
+
+            ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+      }
+
+      /* Define a style for each list item */
+      li {
+        list-style-type: circle;
+        padding: 10px;
+        margin-bottom: 10px;
+        background-color: rgb(222, 182, 171);
+      }
+    </style>
+  </head>
+  <body>
+    <h1>$name</h1>
+    <ul>
+    $loop_element
+        </ul>
+        </body>
+        </html>""")
+    return template_html.substitute(loop_element=ele_arr, name=name)
+
+
+#  send the content encoded  to client
+def handle_client_request(resource, client_socket):
+    if resource == '/':
+        response = ('HTTP/1.0 200 OK\n\n please enter url').encode()
+
+    elif re.match(r"www\.(\w+\.\w+)(\.\w+)?(\/\w+)*", resource):
+        result_ips = domain_to_ip(resource)
+        response = ('HTTP/1.0 200 OK\n\n' + result_ips).encode()
+
+    elif re.match("^reverse\/+\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", resource):
+        result_domains_name = ip_to_domains(resource[8:])
+        response = ('HTTP/1.0 200 OK\n\n' + result_domains_name).encode()
+    else:
+        response = ('HTTP/1.0 200 OK\n\nplease enter valid url, you send: ' + resource).encode()
+    client_socket.send(response)
+
+
+def handle_client(client_socket):
+    while True:
+        # Get the client request
+        request = client_socket.recv(1024).decode()
+        headers = request.split("\n")
+        h0 = headers[0]
+        idx_end_of_resource = h0.find("HTTP") - 1
+        resource = h0[5:idx_end_of_resource]
+        handle_client_request(resource, client_socket)
+        break
+    client_socket.close()
+
+
+def main():
+    # Open a socket and loop forever while waiting for clients
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((MY_IP, MY_PORT))
+    server_socket.listen()
+    print("Listening for connections on port {}".format(MY_PORT))
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_socket.settimeout(SOCKET_TIMEOUT)
+        handle_client(client_socket)
+
+
+if __name__ == "__main__":
+    # Call the main handler function
+    main()
--- temp_tests.py
+++ temp_tests.py
@@ -1,20 +1,112 @@
-import msvcrt
+#! /usr/bin/env python3
+
+from scapy.all import DNS, DNSQR,DNSRR, IP, sr1, UDP
+
+# dns_req = IP(dst='8.8.8.8') / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qtype="PTR", qname="216.58.211.206"))
+# res = sr1(dns_req)
+# print(res[DNS].show())
+
+
+# answer = sr1(dns_req)
+
+# print(answer[DNS].show())
+
+
+def reverse_ip(ip):
+    # Split the IP address into a list of octets
+    octets = ip.split('.')
+
+    # Reverse the list of octets
+    octets.reverse()
+
+    # Join the octets into a reversed IP string
+    reversed_ip = '.'.join(octets)
+
+    # Append ".in-addr.arpa" to the reversed IP
+    reversed_ip += '.in-addr.arpa'
+
+    return reversed_ip
+
+
+from scapy.all import *
+def ip_to_domains(ip):
+    # Set the DNS server to use
+    dns_server = "8.8.8.8"
+    r_ip = reverse_ip(ip)
+    # Build the DNS query packet
+    packet = IP(dst=dns_server) / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=r_ip,qtype="PTR"))
+
+    # Send the packet and get the response
+    try:
+        response = sr1(packet, verbose=0)
+    except:
+        response = ""
+    # response.show()
+    response.show()
+    res = []
+    size = response.ancount
+    # response.show()
+    for i in range(0, size):
+        if response.an[i].type == 12:
+            res.append(response.an[i].rdata.decode())
+    print(res)
+
+
+
+#packet = IP(dst='google.com') / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname="google.com"))
+
+#print(packet.dst)
+
+# ip_to_domains("216.58.205.206")
+ip_to_domains("216.58.205.206")
 
 
 
 
-# def get_usr_input():
-#     print("please enter cmd:")
-#     res = ""
-#     while True:
-#         if msvcrt.kbhit():
-#             c = msvcrt.getch().decode("ASCII")
-#             print(c,end="",flush=True)
-#             if c == '\r':
-#                 break
-#             res += c
-#     return res
+#for i in range(len(response.an)) :
+ #   print(response.an[i])
+# x = response[DNS].an
+# print((x.rdata))
+
+#x.show()
+# def ip_to_domains(ip):
+#     dns_server = "8.8.8.8"
+#     # Build the DNS query packet
+#     packet = IP(dst=dns_server) / UDP(dport=53) / DNS(rd=1, an=DNSRR(rdata=ip))
+#
+#     # Send the packet and get the response
+#     response = sr1(packet, verbose=0)
+#     res = []
+#     # Print the IPs of the domain
+#     for ele in response[DNS].an:
+#         res.append(ele.rdata)
+#     return create_nice_html(res, "List of IPs:")
 
 #
-# name = get_usr_input()
-# print( "HY " , name)
+# def generate_html_template(title, content):
+#     html_template = f"""
+#     <html>
+#         <head>
+#             <title>{title}</title>
+#         </head>
+#         <body>
+#             {content}
+#         </body>
+#     </html>
+#     """
+#     return html_template
+
+
+#
+# import scapy.all as scapy
+#
+# def get_domain(ip):
+#     try:
+#         ans, unans = scapy.sr(scapy.DNSQR(qname=ip))
+#         for s, r in ans:
+#             return r.rrname
+#     except Exception as e:
+#         print(e)
+#         return None
+#
+# print(get_domain( "172.217.16.132"))
